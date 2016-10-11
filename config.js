474ae2cb564d04b56845e12f371cde4554a04483
jarakkal <jarakkal@users.noreply.github.com>
2016-10-12 03:05:52 +1100

    init repo

---
+++ config.js
@@ -0,0 +1,5 @@
+exports.traffic = {
+  lights:[{ title: "North", id: 1, stat: "red" }, { title: "South", id: 2, stat: "red"}, { title: "East", id: 3, stat: "red"}, { title: "West", id: 4, stat: "red"}],
+  timeToLog:{start:'09:00:00',end:'09:30:00'},
+  lightsTimer:{yellow:30000,green:270000}
+};
