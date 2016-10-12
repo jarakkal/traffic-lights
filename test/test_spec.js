18666c5fe73c108fb067714a12749c44d16fb46a
jarakkal <jarakkal@users.noreply.github.com>
2016-10-13 00:44:29 +1100

    add testcases
    added test cases for logging and lights changing

--- test/test_spec.js
+++ test/test_spec.js
@@ -1,11 +1,47 @@
 var chai = require('chai');
 var expect = chai.expect; // we are using the "expect" style of Chai
 var lights = require(__dirname + '/../lights.js');
+var logger = require('../logger.js');
 
-describe('change lights', function() {
-  it('getSubtotal() should return 0 if no items are passed in', function() {
-    var light = new lights();
-    
-    expect(light.setRed(1,5000)).to.equal({id:1,addCol:'green',remCol:'red,yellow'});
-  });
+describe('Logger', function() {  
+    var log = new logger();
+    describe("add logs",function(){
+        it("should add the given msg into database", function(done) {
+            log.add("TestMsg", function(err, insertID){
+                expect(insertID).to.be.a("number");
+                log.delete(insertID, function(err,stat){
+                });
+                done();
+            });
+        });
+    });
+    describe("get logs",function(){
+        it("should extract logs for a given timeframe", function(done) {
+            log.get({start:'00:00:00',end:'00:00:00'}, function(err,rows){
+                expect(rows).to.be.empty;
+                done();
+            });
+        });
+    });
+});
+
+describe('Lights', function() {  
+    describe("set to green",function(){
+        var light = new lights();
+        it("should change the lights to green", function(done) {
+            light.setGreen(1,500,function(params){
+                expect(params).to.deep.equal({id:1,addCol:'green',remCol:'red,yellow'});
+                done();
+            })
+        });
+    });
+    describe("set to red",function(){
+        var light = new lights();
+        it("should change the lights to red", function(done) {
+            light.setRed(2,1000,function(params){
+                expect(params).to.deep.equal({id:2,addCol:'red',remCol:'green,yellow'});
+                done();
+            })
+        });
+    });
 });
